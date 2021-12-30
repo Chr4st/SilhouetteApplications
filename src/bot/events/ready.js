0cb5b92a724158cafd4fbb5b04e8798c0eee84ea
Chr4st <84637278+Chr4st@users.noreply.github.com>
2021-12-30 17:14:02 +0100

    Update ready.js

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -5,6 +5,6 @@ module.exports = async function(client) {
     console.log(chalk.yellow.bold(figlet.textSync("Silhouette")));
     await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold(" Is Ready"));
 
-    await client.user.setActivity(require('../../config/bot').prefix + "help || Silhouette Applications");
+    await client.user.setActivity("!help || Silhouette Applications");
     await client.user.setStatus("online");
 }
